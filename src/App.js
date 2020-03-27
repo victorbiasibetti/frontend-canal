af68ff157254c0d53af2ca6aa3edb15babafc016
Victor Biasibetti <victorbiasibetti7@gmail.com>
2020-03-27 16:31:47 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import api from './services/api'
 
 function App() {
+  const [data, setData] = useState('');
+
+
+  useEffect(() => {
+    const loadData = async () => {
+      const response = await api.get('/')
+      setData(response.data.msg)
+    }
+    loadData();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{data}</h1>      
     </div>
   );
 }
